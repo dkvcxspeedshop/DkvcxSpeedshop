5420e9c091c2f731f1c7e15fa9437799f71ab3ee
dkvcxspeedshop <dchoovacx@gmail.com>
2026-06-28 10:15:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. UBAH NOMOR WHATSAPP ANDA DI SINI (Gunakan kode negara tanpa tanda +)
+const WHATSAPP_NUMBER = "6281234567890"; 
+
+// 2. TEMPAT MENGUBAH / MENAMBAH / MENGHAPUS PRODUK ANDA
+const DATA_PRODUK = {
+    "matic": [
+        {
+            "nama": "Knalpot Tsukigi Mio",
+            "harga": "10.000",
+            "gambar": "GANTI_DENGAN_LINK_FOTO_ANDA.jpg",
+            "spek": "DFF ONLY<br>POLY 800<br>SIZE 500KB<br>FIX TEXTURE"
+        }
+    ],
+    "non-matic": [
+        {
+            "nama": "BAN PIRELLI DIABLO ROSSA",
+            "harga": "10.000",
+            "gambar": "GANTI_DENGAN_LINK_FOTO_ANDA.jpg", 
+            "spek": "DFF, OBJ<br>POLY 1.5K<br>SIZE 2MB<br>HIGH REF"
+        },
+        {
+            "nama": "KUPINGAN NINJA CNC FJN",
+            "harga": "10.000",
+            "gambar": "GANTI_DENGAN_LINK_FOTO_ANDA.jpg", 
+            "spek": "DFF, OBJ<br>POLY 1K<br>SIZE 1MB<br>FIX REFF"
+        }
+    ]
+};
